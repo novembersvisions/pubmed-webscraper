57e32cc179a547b1ffb96e2799734c7792284898
novembersvisions <hprice@carboncopies.org>
2022-07-06 08:10:35 -0400

    Create wbe2.py

---
+++ wbe2.py
@@ -0,0 +1,40 @@
+import requests
+from bs4 import BeautifulSoup
+import time
+
+url = "https://pubmed.ncbi.nlm.nih.gov/?term=neuromorphic&sort=date"
+response = requests.get(url)
+
+soup = BeautifulSoup(response.text, 'html.parser')
+title = soup.get_text()
+#title = soup.find_all("a", {"class": "docsum-title"})
+
+print(title)
+
+# send email
+
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+from emaildetails import email
+from emaildetails import password
+
+send_to_email = 'hprice@carboncopies.org'
+subj = 'PubMed Search Titles'
+
+msg = MIMEMultipart()
+msg['From'] = email
+msg['To'] = send_to_email
+msg['Subject'] = subj
+
+msg.attach(MIMEText(title))
+
+server = smtplib.SMTP('smtp.gmail.com', 587)
+server.starttls()
+server.login(email, password)
+
+mailtxt = msg.as_string()
+server.sendmail(email, send_to_email, mailtxt)
+server.quit()
+
